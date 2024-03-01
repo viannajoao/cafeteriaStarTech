84778ce1dfe9c12249ccb447476b66edc9e7a0b0
Joao Vianna <joovictorvianna@hotmail.com>
2024-02-29 23:06:41 -0300

    fazendo a parte de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent, FormsModule, HttpClientModule ],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, FormsModule, HttpClientModule, CadastroComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { MainComponent } from './components/main/main.component';
+import { NgModule } from '@angular/core';
 
 
-export const routes: Routes = [];
+ export const routes: Routes = [
+  {
+    path: 'cadastrar',
+    component: CadastroComponent
+  },
+  {
+    path: '',
+    component: MainComponent
+  }
+];
+
+
