6c3e2cc678257aff0f1b307e41e88e313363a4ac
Joao Vianna <joovictorvianna@hotmail.com>
2024-03-02 21:50:40 -0300

    criando mensagens de erro

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,13 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule,  } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Client } from '../../models/Client';
+import { ClientService } from '../../servico/client.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule,],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+
+  client = new Client();
+
+ 
+  constructor(private service: ClientService, private snackBar: MatSnackBar) { }
+
+  cadastrar(): void {
+
+
+    this.client.cpf.toString();
+    this.client.tel.toString();
+
+    if (this.client.cpf && this.client.tel && this.client.name && this.client.email != '') {
+      const add = this.service.cadastrar(this.client)
+
+      add.subscribe(retorno =>  this.onSucess(), error => this.onCpf() );
+     
+        console.log(this.client)
+
+      this.client = new Client();
+    }else{
+        this.onErro()
+    }
+
+  }
+
+  onSucess(){
+    this.snackBar.open("Usuario cadastrado com sucesso", '', {
+      duration: 5000
+    })
+  }
+
+  onErro(){
+    this.snackBar.open("Preencha todos os campos para efetuar o cadastrado corretamente", '', {
+      duration: 5000
+    })
+  }
+
+  onCpf(){
+    this.snackBar.open("CPF invalido ou E-mail ja cadastrado", '', {duration: 3000})
+  }
+
 
 }
--- src/app/servico/client.service.ts
+++ src/app/servico/client.service.ts
@@ -8,11 +8,16 @@ import { Client } from '../models/Client';
 })
 export class ClientService {
 
-  private url:string = 'http://localhost:8085';
+  private url:string = 'http://localhost:8080';
+  private urlPost:string = 'http://localhost:8080/cadastrar';
 
   constructor(private http:HttpClient) { }
 
   selecionar():Observable<Client[]>{
       return this.http.get<Client[]>(this.url)
+  }
+
+  cadastrar(obj:Client):Observable<Client>{
+    return this.http.post<Client>(this.urlPost, obj)
   }
 }
